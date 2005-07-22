8fe0e90b6a5c1da689edcf50676481ffb5349d87
root <root@xa-s05.(none)>
2005-07-22 18:01:02 +0200

    [multipathd] fix daemon crash in enable_group() be pp->pgindex is nul
    if a path is added through uev_add_path, pgindex can be unset.
    
    we can safely skip enable_group() in this case, because upon next map
    reload, all PG will be enabled.

--- multipathd/main.c
+++ multipathd/main.c
@@ -979,6 +979,16 @@ enable_group(struct path * pp)
 {
 	struct pathgroup * pgp;
 
+	/*
+	 * if path is added through uev_add_path, pgindex can be unset.
+	 * next update_strings() will set it, upon map reload event.
+	 *
+	 * we can safely return here, because upon map reload, all
+	 * PG will be enabled.
+ 	 */
+	if (!pp->pgindex)
+		return;
+
 	pgp = VECTOR_SLOT(pp->mpp->pg, pp->pgindex - 1);
 	
 	if (pgp->status == PGSTATE_DISABLED) {
