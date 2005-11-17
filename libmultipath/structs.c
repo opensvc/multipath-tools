326a714879733096afc42697734e5d07dbf58bc9
Christophe Varoqui <root@xa-s05.(none)>
2005-11-17 17:49:13 +0100

    [libmultipath] fix pathcount wildcard

--- libmultipath/structs.c
+++ libmultipath/structs.c
@@ -311,7 +311,7 @@ pathcount (struct multipath * mpp, int state)
 
 	vector_foreach_slot (mpp->pg, pgp, i)
 		vector_foreach_slot (pgp->paths, pp, j)
-			if ((pp->state == state) || (state == PATH_WILD))
+			if ((pp->state == state) || (state < 0))
 				count++;
 
 	return count;
