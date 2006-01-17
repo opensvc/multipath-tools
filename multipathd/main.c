9f00a16333c2b1377db4f18cc0b6bb733af203e9
Christophe Varoqui <root@xa-s05.(none)>
2006-01-17 10:13:29 +0100

    [multipathd] "show topo" as a "show maps topo" alias

--- multipathd/main.c
+++ multipathd/main.c
@@ -992,6 +992,7 @@ uxlsnrloop (void * ap)
 	add_handler(LIST+MAPS+STATUS, cli_list_maps_status);
 	add_handler(LIST+MAPS+STATS, cli_list_maps_stats);
 	add_handler(LIST+MAPS+TOPOLOGY, cli_list_maps_topology);
+	add_handler(LIST+TOPOLOGY, cli_list_maps_topology);
 	add_handler(LIST+MAP+TOPOLOGY, cli_list_map_topology);
 	add_handler(LIST+CONFIG, cli_list_config);
 	add_handler(ADD+PATH, cli_add_path);
