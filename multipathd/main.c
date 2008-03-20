6905898a1995710a74c9eb6b6610dc5c73ab557d
Hannes Reinecke <hare@suse.de>
2008-03-20 10:50:40 +0100

    Synchronize dm states for IO errors
    When a real hardware IO error occurs, the path checker will return
    PATH_UP, causing the dm state from the kernel never to be updated.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- multipathd/main.c
+++ multipathd/main.c
@@ -1059,8 +1059,12 @@ check_path (struct vectors * vecs, struct path * pp)
 			enable_group(pp);
 	}
 	else if (newstate == PATH_UP || newstate == PATH_GHOST) {
-		LOG_MSG(4, checker_message(&pp->checker));
-		if (pp->checkint != conf->max_checkint) {
+		if (pp->dmstate == PSTATE_FAILED ||
+		    pp->dmstate == PSTATE_UNDEF) {
+			/* Clear IO errors */
+			reinstate_path(pp, 0);
+		} else {
+			LOG_MSG(4, checker_message(&pp->checker));
 			/*
 			 * double the next check delay.
 			 * max at conf->max_checkint
