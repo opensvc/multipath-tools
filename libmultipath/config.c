ef341c2fa151b1c5b8ba26633fc28916161a85ff
Mike Snitzer <snitzer@redhat.com>
2009-04-28 23:50:21 +0200

    [lib] fix double frees
    A careless merge from Hannes' repo introduced double frees.

--- libmultipath/config.c
+++ libmultipath/config.c
@@ -158,11 +158,6 @@ free_hwe (struct hwentry * hwe)
 	if (hwe->bl_product)
 		FREE(hwe->bl_product);
 
-	if (hwe->prio_name)
-		FREE(hwe->prio_name);
-
-	if (hwe->checker_name)
-		FREE(hwe->checker_name);
 	FREE(hwe);
 }
 
@@ -403,12 +398,6 @@ free_config (struct config * conf)
 
 	if (conf->hwhandler)
 		FREE(conf->hwhandler);
-
-	if (conf->prio_name)
-		FREE(conf->prio_name);
-
-	if (conf->checker_name)
-		FREE(conf->checker_name);
 
 	if (conf->prio_name)
 		FREE(conf->prio_name);
