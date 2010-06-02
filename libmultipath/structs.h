9aa688ee3a3bae7c97bc30b3dcd102d025a43d1a
Hannes Reinecke <hare@suse.de>
2010-06-02 12:49:37 +0200

    libmultipath: check argument length in execute_program()
    The 'path' argument of execute_program() is actually an
    array with fixed length. So we should be using the same
    length here to avoid overflows. And we should check the
    number or arguments, too, as the list we're using is
    static, too.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/structs.h
+++ libmultipath/structs.h
@@ -9,7 +9,7 @@
 #define PATH_STR_SIZE  		16
 #define PARAMS_SIZE		1024
 #define FILE_NAME_SIZE		256
-#define CALLOUT_MAX_SIZE	128
+#define CALLOUT_MAX_SIZE	256
 #define BLK_DEV_SIZE		33
 #define PATH_SIZE		512
 #define NAME_SIZE		512
