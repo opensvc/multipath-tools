20122c8691d8833dbef64d21957c5e1dcce12278
Hannes Reinecke <hare@suse.de>
2010-06-18 12:32:21 +0200

    Set geometry information for multipath maps
    Some programs (most notably grub) try to get the device geometry
    information via the HDIO_GETGEO ioctl. While device-mapper provides
    this ioctl, it's values have to be set previously.
    So we can just use the geometry information from the first path
    to set this information for the multipath map.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/devmapper.c
+++ libmultipath/devmapper.c
@@ -1133,3 +1133,51 @@ out:
 	dm_task_destroy(dmt);
 	return r;
 }
+
+int dm_setgeometry(struct multipath *mpp)
+{
+	struct dm_task *dmt;
+	struct path *pp;
+	char heads[4], sectors[4];
+	char cylinders[10], start[32];
+	int r = 0;
+
+	if (!mpp)
+		return 1;
+
+	pp = first_path(mpp);
+	if (!pp) {
+		condlog(3, "%s: no path for geometry", mpp->alias);
+		return 1;
+	}
+	if (pp->geom.cylinders == 0 ||
+	    pp->geom.heads == 0 ||
+	    pp->geom.sectors == 0) {
+		condlog(3, "%s: invalid geometry on %s", mpp->alias, pp->dev);
+		return 1;
+	}
+
+	if (!(dmt = dm_task_create(DM_DEVICE_SET_GEOMETRY)))
+		return 0;
+
+	if (!dm_task_set_name(dmt, mpp->alias))
+		goto out;
+
+	dm_task_no_open_count(dmt);
+
+	/* What a sick interface ... */
+	snprintf(heads, 4, "%u", pp->geom.heads);
+	snprintf(sectors, 4, "%u", pp->geom.sectors);
+	snprintf(cylinders, 10, "%u", pp->geom.cylinders);
+	snprintf(start, 32, "%lu", pp->geom.start);
+	if (!dm_task_set_geometry(dmt, cylinders, heads, sectors, start)) {
+		condlog(3, "%s: Failed to set geometry", mpp->alias);
+		goto out;
+	}
+
+	r = dm_task_run(dmt);
+out:
+	dm_task_destroy(dmt);
+
+	return r;
+}
