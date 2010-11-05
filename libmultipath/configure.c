5d599d61e3e4744fd82d41559956415f5c83bf69
Benjamin Marzinski <bmarzins@redhat.com>
2010-11-04 23:59:46 -0500

    multipath: fix multipath locking
    In lock_multipath(), if multipathd fails halfway though locking the the path
    devices, it doesn't unlock the ones that it already has locked.  This
    patch fixes that.
    
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>

--- libmultipath/configure.c
+++ libmultipath/configure.c
@@ -284,6 +284,7 @@ lock_multipath (struct multipath * mpp, int lock)
 	struct pathgroup * pgp;
 	struct path * pp;
 	int i, j;
+	int x, y;
 
 	if (!mpp || !mpp->pg)
 		return 0;
@@ -294,12 +295,25 @@ lock_multipath (struct multipath * mpp, int lock)
 		vector_foreach_slot(pgp->paths, pp, j) {
 			if (lock && flock(pp->fd, LOCK_EX | LOCK_NB) &&
 			    errno == EWOULDBLOCK)
-				return 1;
+				goto fail;
 			else if (!lock)
 				flock(pp->fd, LOCK_UN);
 		}
 	}
 	return 0;
+fail:
+	vector_foreach_slot (mpp->pg, pgp, x) {
+		if (x > i)
+			return 1;
+		if (!pgp->paths)
+			continue;
+		vector_foreach_slot(pgp->paths, pp, y) {
+			if (x == i && y >= j)
+				return 1;
+			flock(pp->fd, LOCK_UN);
+		}
+	}
+	return 1;
 }
 
 /*
