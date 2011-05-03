openSUSE/multipath-tools-pre2021

kpartx: Add option '-f' to force devmap creation
Under some circumstances the user needs to override the
'no_partitions' feature of a multipath device.
This patch adds an option '-f' to facilitate this.

References: bnc#613584

Signed-off-by: Libor Pechacek <lpechacek@novell.com>
Acked-by: Hannes Reinecke <hare@suse.de>

kpartx/kpartx.c
@@ -82,20 +82,23 @@ initpts(void)
 	addpts("sun", read_sun_pt);
 }
 
-static char short_opts[] = "ladgvp:t:u";
+static char short_opts[] = "ladfgvp:t:u";
 
 /* Used in gpt.c */
 int force_gpt=0;
 
+int force_devmap=0;
+
 static int
 usage(void) {
-	printf("usage : kpartx [-a|-d|-l] [-v] wholedisk\n");
+	printf("usage : kpartx [-a|-d|-l] [-f] [-v] wholedisk\n");
 	printf("\t-a add partition devmappings\n");
 	printf("\t-d del partition devmappings\n");
 	printf("\t-u update partition devmappings\n");
 	printf("\t-l list partitions devmappings that would be added by -a\n");
 	printf("\t-p set device name-partition number delimiter\n");
 	printf("\t-g force GUID partition table (GPT)\n");
+ 	printf("\t-f force devmap create\n");
 	printf("\t-v verbose\n");
 	return 1;
 }
@@ -231,6 +234,9 @@ main(int argc, char **argv){
 	}
 
 	while ((arg = getopt(argc, argv, short_opts)) != EOF) switch(arg) {
+		case 'f':
+			force_devmap=1;
+			break;
 		case 'g':
 			force_gpt=1;
 			break;
@@ -321,7 +327,8 @@ main(int argc, char **argv){
 
 	if (!mapname) {
 		mapname = device + off;
-	} else if (dm_no_partitions((unsigned int)MAJOR(buf.st_rdev),
+	} else if (!force_devmap &&
+		   dm_no_partitions((unsigned int)MAJOR(buf.st_rdev),
 				  (unsigned int)MINOR(buf.st_rdev))) {
 		/* Feature 'no_partitions' is set, return */
 		return 0;

opensvc/multipath-tools

kpartx/kpartx.c
@@ -82,14 +82,16 @@ initpts(void)
 	addpts("sun", read_sun_pt);
 }
 
-static char short_opts[] = "rladgvp:t:su";
+static char short_opts[] = "rladfgvp:t:su";
 
 /* Used in gpt.c */
 int force_gpt=0;
 
+int force_devmap=0;
+
 static int
 usage(void) {
-	printf("usage : kpartx [-a|-d|-l] [-v] wholedisk\n");
+	printf("usage : kpartx [-a|-d|-l] [-f] [-v] wholedisk\n");
 	printf("\t-a add partition devmappings\n");
 	printf("\t-r devmappings will be readonly\n");
 	printf("\t-d del partition devmappings\n");
@@ -97,6 +99,7 @@ usage(void) {
 	printf("\t-l list partitions devmappings that would be added by -a\n");
 	printf("\t-p set device name-partition number delimiter\n");
 	printf("\t-g force GUID partition table (GPT)\n");
+	printf("\t-f force devmap create\n");
 	printf("\t-v verbose\n");
 	printf("\t-s sync mode. Don't return until the partitions are created\n");
 	return 1;
@@ -238,6 +241,8 @@ main(int argc, char **argv){
 	while ((arg = getopt(argc, argv, short_opts)) != EOF) switch(arg) {
 		case 'r':
 			ro=1;
+		case 'f':
+			force_devmap=1;
 			break;
 		case 'g':
 			force_gpt=1;
@@ -336,7 +341,8 @@ main(int argc, char **argv){
 
 	if (!mapname)
 		mapname = device + off;
-	else if (dm_no_partitions((unsigned int)MAJOR(buf.st_rdev),
+	else if (!force_devmap &&
+		 dm_no_partitions((unsigned int)MAJOR(buf.st_rdev),
 				  (unsigned int)MINOR(buf.st_rdev))) {
 		/* Feature 'no_partitions' is set, return */
 		return 0;