9c78058e2d9e4ee2e197b65a8c2a59ef5d31718b
Benjamin Marzinski <bmarzins@redhat.com>
2012-05-18 17:33:26 -0500

    multipath: fix select_no_path_retry for flushing devices.
    The select_no_path_retry code was falling through if a flush was
    in progress, and so it wasn't honoring flush_on_last_del.
    
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>

--- libmultipath/propsel.c
+++ libmultipath/propsel.c
@@ -415,6 +415,7 @@ select_no_path_retry(struct multipath *mp)
 	if (mp->flush_on_last_del == FLUSH_IN_PROGRESS) {
 		condlog(0, "flush_on_last_del in progress");
 		mp->no_path_retry = NO_PATH_RETRY_FAIL;
+		return 0;
 	}
 	if (mp->mpe && mp->mpe->no_path_retry != NO_PATH_RETRY_UNDEF) {
 		mp->no_path_retry = mp->mpe->no_path_retry;
