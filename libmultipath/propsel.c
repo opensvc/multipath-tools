3baf448656f3688e8a395f79a3cb24f6fec07066
Hannes Reinecke <hare@suse.de>
2013-01-08 14:53:52 +0100

    Print out multipath alias for flush_on_last_del messages
    Added for consistency.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/propsel.c
+++ libmultipath/propsel.c
@@ -606,24 +606,25 @@ select_flush_on_last_del(struct multipath *mp)
 		return 0;
 	if (mp->mpe && mp->mpe->flush_on_last_del != FLUSH_UNDEF) {
 		mp->flush_on_last_del = mp->mpe->flush_on_last_del;
-		condlog(3, "flush_on_last_del = %i (multipath setting)",
-				mp->flush_on_last_del);
+		condlog(3, "%s: flush_on_last_del = %i (multipath setting)",
+			mp->alias, mp->flush_on_last_del);
 		return 0;
 	}
 	if (mp->hwe && mp->hwe->flush_on_last_del != FLUSH_UNDEF) {
 		mp->flush_on_last_del = mp->hwe->flush_on_last_del;
-		condlog(3, "flush_on_last_del = %i (controler setting)",
-				mp->flush_on_last_del);
+		condlog(3, "%s: flush_on_last_del = %i (controler setting)",
+			mp->alias, mp->flush_on_last_del);
 		return 0;
 	}
 	if (conf->flush_on_last_del != FLUSH_UNDEF) {
 		mp->flush_on_last_del = conf->flush_on_last_del;
-		condlog(3, "flush_on_last_del = %i (config file default)",
-				mp->flush_on_last_del);
+		condlog(3, "%s: flush_on_last_del = %i (config file default)",
+			mp->alias, mp->flush_on_last_del);
 		return 0;
 	}
 	mp->flush_on_last_del = FLUSH_UNDEF;
-	condlog(3, "flush_on_last_del = DISABLED (internal default)");
+	condlog(3, "%s: flush_on_last_del = DISABLED (internal default)",
+		mp->alias);
 	return 0;
 }
 
