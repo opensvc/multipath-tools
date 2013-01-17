024880a13daee2c3d06b91718aa80be93b77c6a3
Hannes Reinecke <hare@suse.de>
2013-01-17 15:59:30 +0100

    multipathd: log message when check interval has changed
    We should only log the message if the check interval has
    indeed changed.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- multipathd/main.c
+++ multipathd/main.c
@@ -1209,18 +1209,20 @@ check_path (struct vectors * vecs, struct path * pp)
 			reinstate_path(pp, 0);
 		} else {
 			LOG_MSG(4, checker_message(&pp->checker));
-			/*
-			 * double the next check delay.
-			 * max at conf->max_checkint
-			 */
-			if (pp->checkint < (conf->max_checkint / 2))
-				pp->checkint = 2 * pp->checkint;
-			else
-				pp->checkint = conf->max_checkint;
+			if (pp->checkint != conf->max_checkint) {
+				/*
+				 * double the next check delay.
+				 * max at conf->max_checkint
+				 */
+				if (pp->checkint < (conf->max_checkint / 2))
+					pp->checkint = 2 * pp->checkint;
+				else
+					pp->checkint = conf->max_checkint;
 
-			pp->tick = pp->checkint;
-			condlog(4, "%s: delay next check %is",
-				pp->dev_t, pp->tick);
+				pp->tick = pp->checkint;
+				condlog(4, "%s: delay next check %is",
+					pp->dev_t, pp->tick);
+			}
 		}
 	}
 	else if (newstate == PATH_DOWN) {
