ca985dfab4b0900f11757882c078503f0a5ccd20
Hannes Reinecke <hare@suse.de>
2013-11-26 12:41:25 +0100

    multipathd: switch to socket activation for systemd
    multipathd already has a netlink socket for CLI commands, which
    can be used for socket activation.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/uxsock.c
+++ libmultipath/uxsock.c
@@ -16,6 +16,9 @@
 #include <sys/poll.h>
 #include <signal.h>
 #include <errno.h>
+#ifdef USE_SYSTEMD
+#include <systemd/sd-daemon.h>
+#endif
 
 #include "memory.h"
 #include "uxsock.h"
@@ -57,8 +60,22 @@ int ux_socket_connect(const char *name)
 int ux_socket_listen(const char *name)
 {
 	int fd, len;
+#ifdef USE_SYSTEMD
+	int num;
+#endif
 	struct sockaddr_un addr;
 
+#ifdef USE_SYSTEMD
+	num = sd_listen_fds(0);
+	if (num > 1) {
+		condlog(3, "sd_listen_fds returned %d fds", num);
+		return -1;
+	} else if (num == 1) {
+		fd = SD_LISTEN_FDS_START + 0;
+		condlog(3, "using fd %d from sd_listen_fds", fd);
+		return fd;
+	}
+#endif
 	fd = socket(AF_LOCAL, SOCK_STREAM, 0);
 	if (fd == -1) {
 		condlog(3, "Couldn't create ux_socket, error %d", errno);
