355291bc68fb9c132b9a9e3abf37b35974e19d4f
Hannes Reinecke <hare@suse.de>
2014-02-13 16:33:27 +0100

    libmultipath: filter for missing property in get_refwwid()
    get_refwwid() needs to filter for missing udev properties,
    otherwise it'll accept just about any SCSI device.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/configure.c
+++ libmultipath/configure.c
@@ -962,6 +962,9 @@ get_refwwid (char * dev, enum devtypes dev_type, vector pathvec, char **wwid)
 				return ret;
 			}
 		}
+		if (pp->udev && filter_property(conf, pp->udev) > 0)
+			return 2;
+
 		refwwid = pp->wwid;
 		goto out;
 	}
@@ -986,6 +989,9 @@ get_refwwid (char * dev, enum devtypes dev_type, vector pathvec, char **wwid)
 				return ret;
 			}
 		}
+		if (pp->udev && filter_property(conf, pp->udev) > 0)
+			return 2;
+
 		refwwid = pp->wwid;
 		goto out;
 	}
