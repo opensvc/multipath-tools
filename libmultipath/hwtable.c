2d74e0154ac62a232ee282bb357ba74ca316642e
Xose Vazquez Perez <xose.vazquez@gmail.com>
2016-06-13 16:38:50 +0200

    multipath-tools: add TEGILE devices to hardware table
    based on documentation provided by manufacturer:
    http://pages.tegile.com/rs/568-BVY-995/images/Oracle%20BPG%201.0.pdf
    
    vendor                  "TEGILE"
    product                 "ZEBI-FC|INTELLIFLASH"
    hardware_handler        "1 alua"
    path_selector           "round- robin 0"
    path_grouping_policy    "group_by_prio"
    no_path_retry           10
    dev_loss_tmo            50
    path_checker            tur
    prio                    alua
    failback                30
    rr_min_io               128
    
    Cc: Benjamin Marzinski <bmarzins@redhat.com>
    Cc: Hannes Reinecke <hare@suse.de>
    Cc: Christophe Varoqui <christophe.varoqui@opensvc.com>
    Cc: device-mapper development <dm-devel@redhat.com>
    Signed-off-by: Xose Vazquez Perez <xose.vazquez@gmail.com>

--- libmultipath/hwtable.c
+++ libmultipath/hwtable.c
@@ -1176,6 +1176,25 @@ static struct hwentry default_hw[] = {
 		.checker_name  = TUR,
 	},
 	/*
+	 * Tegile Systems, Inc.
+	 *
+	 * Maintainer : Christophe Varoqui
+	 * Mail : christophe.varoqui@opensvc.com
+	 */
+	{
+		.vendor        = "TEGILE",
+		.product       = "ZEBI-FC|INTELLIFLASH",
+		.hwhandler     = "1 alua",
+		.selector      = "round-robin 0",
+		.pgpolicy      = GROUP_BY_PRIO,
+		.no_path_retry = 10,
+		.dev_loss      = 50,
+		.checker_name  = TUR,
+		.prio_name     = PRIO_ALUA,
+		.pgfailback    = 30,
+		.minio         = 128,
+	},
+	/*
 	 * EOL
 	 */
 	{
