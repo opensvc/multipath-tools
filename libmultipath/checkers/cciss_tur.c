5619a39c433ac3d10a88079593cec1aa6472cbeb
Xose Vazquez Perez <xose.vazquez@gmail.com>
2016-06-30 16:39:52 +0200

    multipath-tools: replace FSF address with a www pointer
    less prone to future modifications, new FSF licences point
    exactly to this url: <http://www.gnu.org/licenses/>
    
    Cc: Christophe Varoqui <christophe.varoqui@opensvc.com>
    Cc: device-mapper development <dm-devel@redhat.com>
    Signed-off-by: Xose Vazquez Perez <xose.vazquez@gmail.com>

--- libmultipath/checkers/cciss_tur.c
+++ libmultipath/checkers/cciss_tur.c
@@ -14,11 +14,7 @@
  * for more details.                                                         *
  *                                                                           *
  * You should have received a copy of the GNU General Public License along   *
- * with this program; if not, write to the Free Software Foundation, Inc.,   *
- * 675 Mass Ave, Cambridge, MA 02139, USA.                                   *
- *                                                                           *
- * The copy of the GNU General Public License is available at                *
- * /opt/hp/HPDMmultipath-tool directoy                                       *
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.           *
  *                                                                           *
  *****************************************************************************
 */
