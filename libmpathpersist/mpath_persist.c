558524f840a20cbf8e2d8fa4f7c910109ed63cdb
ding.yemin <ding.yemin@zte.com.cn>
2016-10-31 10:55:07 +0800

    Remove duplicated code in mpath_alloc_prin_response()
    Remove duplicated code in mpath_alloc_prin_response().
    
    Signed-off-by: ding.yemin <ding.yemin@zte.com.cn>

--- libmpathpersist/mpath_persist.c
+++ libmpathpersist/mpath_persist.c
@@ -833,25 +833,21 @@ void * mpath_alloc_prin_response(int prin_sa)
 	{
 		case MPATH_PRIN_RKEY_SA:
 			size = sizeof(struct prin_readdescr);
-			ptr = malloc(size);
-			memset(ptr, 0, size);
 			break;
 		case MPATH_PRIN_RRES_SA:
 			size = sizeof(struct prin_resvdescr);
-			ptr = malloc(size);
-			memset(ptr, 0, size);
 			break;
 		case MPATH_PRIN_RCAP_SA:
 			size=sizeof(struct prin_capdescr);
-			ptr = malloc(size);
-			memset(ptr, 0, size);
 			break;
 		case MPATH_PRIN_RFSTAT_SA:
 			size = sizeof(struct print_fulldescr_list) +
 				sizeof(struct prin_fulldescr *)*MPATH_MX_TIDS;
-			ptr = malloc(size);
-			memset(ptr, 0, size);
 			break;
+	}
+	if (size > 0)
+	{
+		ptr = calloc(size, 1);
 	}
 	return ptr;
 }
