b0c9d7a92c93c2e562d62a11395a4a265dccb295
Bart Van Assche <bart.vanassche@wdc.com>
2018-03-08 00:15:46 +0100

    libmultipath, alloc_path_with_pathinfo(): Ensure that pp->wwid is '\0'-terminated
    Discovered by Coverity (CID 173257).
    
    Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>
    Signed-off-by: Martin Wilck <mwilck@suse.com>

--- libmultipath/discovery.c
+++ libmultipath/discovery.c
@@ -53,8 +53,8 @@ alloc_path_with_pathinfo (struct config *conf, struct udev_device *udevice,
 	if (!pp)
 		return PATHINFO_FAILED;
 
-	if(wwid)
-		strncpy(pp->wwid, wwid, sizeof(pp->wwid));
+	if (wwid)
+		strlcpy(pp->wwid, wwid, sizeof(pp->wwid));
 
 	if (safe_sprintf(pp->dev, "%s", devname)) {
 		condlog(0, "pp->dev too small");
