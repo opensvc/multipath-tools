8a7e9b6648898f2c2d2bbd2d22871f79612e716e
Christophe Varoqui <christophe.varoqui@opensvc.com>
2019-07-03 08:56:58 +0200

    Bump version to 0.8.2

--- libmultipath/version.h
+++ libmultipath/version.h
@@ -20,8 +20,8 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 
-#define VERSION_CODE 0x000801
-#define DATE_CODE    0x041213
+#define VERSION_CODE 0x000802
+#define DATE_CODE    0x070313
 
 #define PROG    "multipath-tools"
 
