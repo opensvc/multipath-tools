1ed430f9d5921a7d669b3f24751f528be1805dd9
Benjamin Marzinski <bmarzins@redhat.com>
2021-05-17 11:29:55 -0500

    multipathd: remove duplicate orphan_paths in flush_map
    remove_map_and_stop_waiter() already calls orphan_paths() so flush_map()
    doesn't need to call orphan_paths() before calling
    remove_map_and_stop_waiter().
    
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>
    Reviewed-by: Martin Wilck <mwilck@suse.com>

--- multipathd/main.c
+++ multipathd/main.c
@@ -660,7 +660,6 @@ flush_map(struct multipath * mpp, struct vectors * vecs, int nopaths)
 	else
 		condlog(2, "%s: map flushed", mpp->alias);
 
-	orphan_paths(vecs->pathvec, mpp, "map flushed");
 	remove_map_and_stop_waiter(mpp, vecs);
 
 	return 0;
