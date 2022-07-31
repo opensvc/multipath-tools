7ae8db246009f4cdc5e9f20bea57af77641630c6
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-07-31 13:11:19 -0400

    spelling: multipathd
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/valid.c
+++ tests/valid.c
@@ -452,7 +452,7 @@ static void test_greedy(void **state)
 	assert_string_equal(pp.dev, name);
 	assert_ptr_equal(pp.udev, &test_udev);
 	assert_string_equal(pp.wwid, wwid);
-	/* test greedy success without checking multiapthd */
+	/* test greedy success without checking multipathd */
 	memset(&pp, 0, sizeof(pp));
 	setup_passing(name, wwid, CHECK_MPATHD_SKIP, STAGE_IS_FAILED);
 	assert_int_equal(is_path_valid(name, &conf, &pp, false),
