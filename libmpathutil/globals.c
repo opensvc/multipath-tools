9ee97aedfd7d3b30ef1c6f8662c1e3741822cca3
Martin Wilck <mwilck@suse.com>
2022-10-25 16:31:46 +0200

    libmpathutil: remove __attribute__((weak))
    Using of __attribute__((weak)) here has no effect for dynamic
    linking.
    
    Quoting from ld.so(8): "Standard practice is that the distinction
    between weak and strong symbols should have effect only at static
    link time." See also the ELF specification, p. 1-18 "Symbol Table".
    
    We would only need this if we supported static linking for
    multipath-tools, which we haven't done for a long time.

--- libmpathutil/globals.c
+++ libmpathutil/globals.c
@@ -2,10 +2,10 @@
 #include <libudev.h>
 #include "globals.h"
 
-struct config __attribute__((weak)) *get_multipath_config(void)
+struct config *get_multipath_config(void)
 {
 	return NULL;
 }
 
-void __attribute__((weak)) put_multipath_config(void *p __attribute__((unused)))
+void put_multipath_config(void *p __attribute__((unused)))
 {}
