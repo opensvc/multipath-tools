opensvc/multipath-tools

commit 55da6082d0eca65a54266095963ed094648dc993

libmultipath: update INFINIDAT builtin config
Based on the manufacturer documentation:
https://support.infinidat.com/hc/en-us/articles/202319222

INFINIDAT recommends round-robin path selector using
a different path per IO. Timeout and path recovery values
are adjusted for error-free hot upgrade scenarios.

Signed-off-by: Arnon Yaari <arnony@infinidat.com>

libmultipath/hwtable.c
@@ -964,13 +964,24 @@ static struct hwentry default_hw[] = {
 	},
 	/*
 	 * Infinidat
+	 *
+	 * Maintainer: Arnon Yaari
+	 * Mail: arnony@infinidat.com
 	 */
 	{
 		.vendor        = "NFINIDAT",
 		.product       = "InfiniBox",
 		.pgpolicy      = GROUP_BY_PRIO,
-		.pgfailback    = -FAILBACK_IMMEDIATE,
+		.pgfailback    = 30,
 		.prio_name     = PRIO_ALUA,
+		.selector      = "round-robin 0",
+		.rr_weight     = RR_WEIGHT_PRIO,
+		.no_path_retry = NO_PATH_RETRY_FAIL,
+		.minio         = 1,
+		.minio_rq      = 1,
+		.flush_on_last_del = FLUSH_ENABLED,
+		.fast_io_fail  = 15,
+		.dev_loss      = 15,
 	},
 	/*
 	 * Nimble Storage

commit d867d9b9a74a363cb14f380e233c15b36fcda71c

libmultipath: hwtable: fix fast_io_fail for Infinibox
The Infinibox hwtable entry had fast_io_fail and dev_loss both set to 15,
which has the effect that fast_io_fail is switched off (dev_loss timeout
must be higher than fast_io_fail timeout).

Remove the dev_loss value, which will cause the default of 600 to be used.
Experience shows that dev_loss_tmo values below a few minutes are seldom
helpful.

Fixes: 55da608 ("libmultipath: update INFINIDAT builtin config")
Signed-off-by: Martin Wilck <mwilck@suse.com>
Cc: Arnon Yaari <arnony@infinidat.com>
Cc: Xose Vazquez Perez <xose.vazquez@gmail.com>
Cc: Tom Abraham <tabraham@suse.com>
Cc: Jiri Belka <jiri.belka@suse.com>
Reviewed-by: Benjamin Marzinski <bmarzins@redhat.com>

libmultipath/hwtable.c
@@ -1162,7 +1162,6 @@ static struct hwentry default_hw[] = {
 		.minio_rq      = 1,
 		.flush_on_last_del = FLUSH_ENABLED,
 		.fast_io_fail  = 15,
-		.dev_loss      = 15,
 	},
 	/*
 	 * Kaminario