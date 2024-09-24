bc68bd805a97c95227b4c29d8a94e7b8a0c916d9
Xose Vazquez Perez <xose.vazquez@gmail.com>
2024-09-25 01:55:22 +0200

    multipath-tools: hwtable housekeeping
    Update some descriptions. And limit the Dorado regex,
    because "Dorado "is used as a generic brand by Huawei.
    
    =====================
    HUASY   Dorado2100
    HUAWEI  Dorado2100
    HUAWEI  Dorado2100 G2
    HUASY   Dorado5100
    HUAWEI  Dorado5100
    =====================
    
    Cc: Martin Wilck <mwilck@suse.com>
    Cc: Benjamin Marzinski <bmarzins@redhat.com>
    Cc: Christophe Varoqui <christophe.varoqui@opensvc.com>
    Cc: DM-DEVEL ML <dm-devel@lists.linux.dev>
    Signed-off-by: Xose Vazquez Perez <xose.vazquez@gmail.com>
    Reviewed-by: Benjamin Marzinski <bmarzins@redhat.com>
    (cherry picked from commit 97b05a7da72251768f9574a455d8ed27cb2e63aa)

--- libmultipath/hwtable.c
+++ libmultipath/hwtable.c
@@ -801,7 +801,7 @@ static struct hwentry default_hw[] = {
 	 */
 	{
 		/*
-		 * ONTAP FAS/AFF Series
+		 * ONTAP FAS/AFF/ASA Series
 		 *
 		 * Maintainer: Martin George <marting@netapp.com>
 		 */
@@ -1074,7 +1074,7 @@ static struct hwentry default_hw[] = {
 	 * Pure Storage
 	 */
 	{
-		/* FlashArray */
+		/* FlashArray family */
 		.vendor        = "PURE",
 		.product       = "FlashArray",
 		.pgpolicy      = GROUP_BY_PRIO,
@@ -1094,15 +1094,15 @@ static struct hwentry default_hw[] = {
 	 * Huawei
 	 */
 	{
-		/* All, except OceanStor V3-V6 */
+		/* Older than OceanStor V3 */
 		.vendor        = "^(HUAWEI|HUASY|HS)",
-		.product       = "^(Dorado|HVS8|S[23568]|V1[568]|VIS6000)",
+		.product       = "^(Dorado[25]|HVS8|S[23568]|V1[568]|VIS6000)",
 		.pgpolicy      = GROUP_BY_PRIO,
 		.pgfailback    = -FAILBACK_IMMEDIATE,
 		.no_path_retry = 15,
 	},
 	{
-		/* OceanStor V3-V6 */
+		/* OceanStor V3 or better */
 		.vendor        = "^(HUAWEI|AnyStor|Marstor|NETPOSA|SanM|SUGON|UDsafe)",
 		.product       = "XSG1",
 		.pgpolicy      = GROUP_BY_PRIO,
