openSUSE/multipath-tools-1

libmultipath: bump version to 0.11.0
Signed-off-by: Martin Wilck <mwilck@suse.com>

libmultipath/version.h
@@ -20,9 +20,9 @@
 #ifndef VERSION_H_INCLUDED
 #define VERSION_H_INCLUDED
 
-#define VERSION_CODE 0x000A00
+#define VERSION_CODE 0x000B00
 /* MMDDYY, in hex */
-#define DATE_CODE    0x080F18
+#define DATE_CODE    0x0B0D18
 
 #define PROG    "multipath-tools"
 

opensvc/multipath-tools

libmultipath/version.h
@@ -20,9 +20,9 @@
 #ifndef VERSION_H_INCLUDED
 #define VERSION_H_INCLUDED
 
-#define VERSION_CODE 0x000A00
+#define VERSION_CODE 0x000B00
 /* MMDDYY, in hex */
-#define DATE_CODE    0x080F18
+#define DATE_CODE    0x110119
 
 #define PROG    "multipath-tools"
 