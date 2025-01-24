opensvc/multipath-tools

commit 90773ba8f164285092f347089c95df0055c98de1

libmultipath: resolve hash collisions in pgcmp()
pgcmp() is using a simple xor algorithm for hashing. Main intention
here is to have a quick comparison if two paths are identical.
However, this algorithm is prone to hash collisions, so even if
two hashes match we should still check the individual paths to
avoid collisions.

Signed-off-by: Hannes Reinecke <hare@suse.de>

libmultipath/configure.c
@@ -132,7 +132,8 @@ pgcmp (struct multipath * mpp, struct multipath * cmpp)
 		compute_pgid(pgp);
 
 		vector_foreach_slot (cmpp->pg, cpgp, j) {
-			if (pgp->id == cpgp->id) {
+			if (pgp->id == cpgp->id &&
+			    !pathcmp(pgp, cpgp)) {
 				r = 0;
 				break;
 			}

libmultipath/structs.c
@@ -371,6 +371,26 @@ pathcount (struct multipath * mpp, int state)
 	return count;
 }
 
+extern int
+pathcmp (struct pathgroup *pgp, struct pathgroup *cpgp)
+{
+	int i, j;
+	struct path *pp, *cpp;
+	int pnum = 0, found = 0;
+
+	vector_foreach_slot(pgp->paths, pp, i) {
+		pnum++;
+		vector_foreach_slot(cpgp->paths, cpp, j) {
+			if ((long)pp == (long)cpp) {
+				found++;
+				break;
+			}
+		}
+	}
+
+	return pnum - found;
+}
+
 struct path *
 first_path (struct multipath * mpp)
 {

libmultipath/structs.h
@@ -251,6 +251,7 @@ struct path * first_path (struct multipath * mpp);
 
 int pathcountgr (struct pathgroup *, int);
 int pathcount (struct multipath *, int);
+int pathcmp (struct pathgroup *, struct pathgroup *);
 void setup_feature(struct multipath *, char *);
 
 extern char sysfs_path[PATH_SIZE];

commit e7fe3a03eb8acc8999eb6d3c8b3ae800f17b2af5

libmultipath: make pgcmp detect if map is missing a path group
The previous algorithm didn't detect the case case where cpgp
contained a path that was not contained in pgp. Fix this.

Cherry-picked from d4b35f61cb75c6e9b289e56c98457fc04ce4835e
Fixes: 90773ba ("libmultipath: resolve hash collisions in pgcmp()")
Signed-off-by: Martin Wilck <mwilck@suse.com>

libmultipath/configure.c
@@ -426,6 +426,11 @@ compute_pgid(struct pathgroup * pgp)
 		pgp->id ^= (long)pp;
 }
 
+static void cleanup_bitfield(struct bitfield **p)
+{
+	free(*p);
+}
+
 static int
 pgcmp (struct multipath * mpp, struct multipath * cmpp)
 {
@@ -433,9 +438,17 @@ pgcmp (struct multipath * mpp, struct multipath * cmpp)
 	struct pathgroup * pgp;
 	struct pathgroup * cpgp;
 	int r = 0;
+	struct bitfield *bf __attribute__((cleanup(cleanup_bitfield))) = NULL;
 
 	if (!mpp)
 		return 0;
+
+	if (VECTOR_SIZE(mpp->pg) != VECTOR_SIZE(cmpp->pg))
+		return 1;
+
+	bf = alloc_bitfield(VECTOR_SIZE(cmpp->pg));
+	if (!bf)
+		return 1;
 
 	vector_foreach_slot (mpp->pg, pgp, i) {
 		compute_pgid(pgp);
@@ -443,6 +456,7 @@ pgcmp (struct multipath * mpp, struct multipath * cmpp)
 		vector_foreach_slot (cmpp->pg, cpgp, j) {
 			if (pgp->id == cpgp->id &&
 			    !pathcmp(pgp, cpgp)) {
+				set_bit_in_bitfield(j, bf);
 				r = 0;
 				break;
 			}
@@ -450,6 +464,10 @@ pgcmp (struct multipath * mpp, struct multipath * cmpp)
 		}
 		if (r)
 			return r;
+	}
+	vector_foreach_slot (cmpp->pg, cpgp, j) {
+		if (!is_bit_set_in_bitfield(j, bf))
+			return 1;
 	}
 	return r;
 }