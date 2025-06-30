af9f4441cafbe07d39b359601749bf9732b4dcbf
Xose Vazquez Perez <xose.vazquez@gmail.com>
2025-06-30 23:52:13 +0200

    multipath-tools: fix licence identifier
    Also location and SLI tag: https://spdx.org/licenses/
    
    Cc: Martin Wilck <mwilck@suse.com>
    Cc: Benjamin Marzinski <bmarzins@redhat.com>
    Cc: Christophe Varoqui <christophe.varoqui@opensvc.com>
    Cc: DM-DEVEL ML <dm-devel@lists.linux.dev>
    Signed-off-by: Xose Vazquez Perez <xose.vazquez@gmail.com>
    Reviewed-by: Martin Wilck <mwilck@suse.com>

--- libmpathcmd/mpath_fill_sockaddr.c
+++ libmpathcmd/mpath_fill_sockaddr.c
@@ -1,6 +1,6 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
 /*
  * Copyright (c) 2025 SUSE LLC
- * SPDX-license-identifier: LGPL-2.1-or-newer
  */
 
 static size_t mpath_fill_sockaddr__(struct sockaddr_un *addr, const char *name)
