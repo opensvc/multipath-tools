46ef09bedcdfc48a7979bfd8b1d969a3efe5448f
Xose Vazquez Perez <xose.vazquez@gmail.com>
2025-07-12 22:14:53 +0200

    multipath-tools: fix default blacklist of s390 devices
    Each blacklist only their own devices.
    
    Cc: Stefan Haberland <sth@linux.ibm.com>
    Cc: Nigel Hislop <hislop_nigel@emc.com>
    Cc: Matthias Rudolph <Matthias.Rudolph@hitachivantara.com>
    Cc: Heiko Carstens <hca@linux.ibm.com>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: Christian Borntraeger <borntraeger@linux.ibm.com>
    Cc: Sven Schnelle <svens@linux.ibm.com>
    Cc: Hannes Reinecke <hare@suse.de>
    Cc: Martin Wilck <mwilck@suse.com>
    Cc: Benjamin Marzinski <bmarzins@redhat.com>
    Cc: Christophe Varoqui <christophe.varoqui@opensvc.com>
    Cc: S390-ML <linux-s390@vger.kernel.org>
    Cc: DM-DEVEL-ML <dm-devel@lists.linux.dev>
    Signed-off-by: Xose Vazquez Perez <xose.vazquez@gmail.com>
    Reviewed-by: Martin Wilck <mwilck@suse.com>
    Reviewed-by: Stefan Haberland <sth@linux.ibm.com>
    (cherry picked from commit c50fe04a9232e151f5df1c1c9dcc06ca86408df3)

--- libmultipath/hwtable.c
+++ libmultipath/hwtable.c
@@ -677,7 +677,7 @@ static struct hwentry default_hw[] = {
 		/* PAV DASD ECKD */
 		.vendor        = "IBM",
 		.product       = "S/390 DASD ECKD",
-		.bl_product    = "S/390",
+		.bl_product    = "S/390 DASD ECKD",
 		.uid_attribute = "ID_UID",
 		.no_path_retry = NO_PATH_RETRY_QUEUE,
 		.pgpolicy      = MULTIBUS,
@@ -687,7 +687,7 @@ static struct hwentry default_hw[] = {
 		/* PAV DASD FBA */
 		.vendor        = "IBM",
 		.product       = "S/390 DASD FBA",
-		.bl_product    = "S/390",
+		.bl_product    = "S/390 DASD FBA",
 		.uid_attribute = "ID_UID",
 		.no_path_retry = NO_PATH_RETRY_QUEUE,
 		.pgpolicy      = MULTIBUS,
