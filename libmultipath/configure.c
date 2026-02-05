opensvc/multipath-tools

commit a04be5570906e1a561794afc5a567cfde579b6f5

multipathd: don't add removed/partial paths to new maps
During configure(), when the old multipath devices are added in
map_discovery(), it's possible that they have paths in the INIT_PARTIAL
or INIT_REMOVED state. These paths were not found during
path_discovery(). Don't add them to the new multipath maps.
coalesce_maps() will make sure that they point to the old maps, so they
will get cleaned up when those maps are removed.

Reviewed-by: Martin Wilck <mwilck@suse.com>
Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>
Reviewed-by: Martin Wilck <mwilck@suse.com>

libmultipath/configure.c
@@ -1113,7 +1113,17 @@ int coalesce_paths (struct vectors *vecs, vector mpvec, char *refwwid,
 			continue;
 		}
 
-		/* 4. path is out of scope */
+		/*
+		 * 4. The path wasn't found in path_discovery. It only exists
+		 *    in an old map.
+		 */
+		if (pp1->initialized == INIT_PARTIAL ||
+		    pp1->initialized == INIT_REMOVED) {
+			orphan_path(pp1, "path not found");
+			continue;
+		}
+
+		/* 5. path is out of scope */
 		if (refwwid && strncmp(pp1->wwid, refwwid, WWID_SIZE - 1))
 			continue;
 

libmultipath/structs_vec.c
@@ -315,7 +315,8 @@ int adopt_paths(vector pathvec, struct multipath *mpp,
 					pp->dev, mpp->alias);
 				continue;
 			}
-			if (pp->initialized == INIT_REMOVED)
+			if (pp->initialized == INIT_REMOVED ||
+			    pp->initialized == INIT_PARTIAL)
 				continue;
 			if (mpp->queue_mode == QUEUE_MODE_RQ &&
 			    pp->bus == SYSFS_BUS_NVME &&

commit 01cc89c46a286168c6f18fe0bf03adaef7be83d9

multipathd: fix failures on booting from a multipath device
Commit a04be557 ("multipathd: don't add removed/partial paths to new
maps") stopped including INIT_PARTIAL paths in the updated multipath
devices built during reconfigure(). During normal multipath operation
this does make sense, but isn't necessary (the uevent should still come
shortly) and the situation is very unlikely to occur. On the other hand,
when multipathd starts up after the pivot root during boot, it's very
possible that paths will not be fully initialized. Multipathd should not
drops paths in this case. The patch mostly reverts a04be557.

Paths that are marked as INIT_REMOVED shouldn't be added in
coalesce_paths(). adopt_paths() was already making sure that they
weren't before a04be557. But it does make sense to catch those paths
earlier in coalesce_paths(), so this patch retains that behavior.

Fixes: a04be557 ("multipathd: don't add removed/partial paths to new maps")
Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>
Reviewed-by: Martin Wilck <mwilck@suse.com>

libmultipath/configure.c
@@ -1108,22 +1108,12 @@ int coalesce_paths (struct vectors *vecs, vector mpvec, char *refwwid,
 			continue;
 
 		/* 3. if path has disappeared */
-		if (pp1->state == PATH_REMOVED) {
+		if (pp1->state == PATH_REMOVED || pp1->initialized == INIT_REMOVED) {
 			orphan_path(pp1, "path removed");
 			continue;
 		}
 
-		/*
-		 * 4. The path wasn't found in path_discovery. It only exists
-		 *    in an old map.
-		 */
-		if (pp1->initialized == INIT_PARTIAL ||
-		    pp1->initialized == INIT_REMOVED) {
-			orphan_path(pp1, "path not found");
-			continue;
-		}
-
-		/* 5. path is out of scope */
+		/* 4. path is out of scope */
 		if (refwwid && strncmp(pp1->wwid, refwwid, WWID_SIZE - 1))
 			continue;
 

libmultipath/structs_vec.c
@@ -315,8 +315,7 @@ int adopt_paths(vector pathvec, struct multipath *mpp,
 					pp->dev, mpp->alias);
 				continue;
 			}
-			if (pp->initialized == INIT_REMOVED ||
-			    pp->initialized == INIT_PARTIAL)
+			if (pp->initialized == INIT_REMOVED)
 				continue;
 			if (mpp->queue_mode == QUEUE_MODE_RQ &&
 			    pp->bus == SYSFS_BUS_NVME &&